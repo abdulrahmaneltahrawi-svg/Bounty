7e7d2e9990b7605eb745574f3fc2e6f8f3cbea96
abdulrahmaneltahrawi-svg <abdulrahmaneltahrawi@gmail.com>
2026-06-23 14:01:04 +0300

    Add files via upload

---
+++ js/scritp.js
@@ -0,0 +1,7 @@
+// تحميل الهيدر من ملف components/header.html
+fetch('components/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+  })
+  .catch(error => console.error('Error loading header:', error));
